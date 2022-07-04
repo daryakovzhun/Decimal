eafc2795120eff74d8e24871534a8ab40ce2254f
Daria Kovzhun <dariakovzhun@gmail.com>
2022-07-04 14:35:14 +0300

    new

--- converter.c
+++ converter.c
@@ -2,14 +2,65 @@
 
 int s21_from_int_to_decimal(int src, s21_decimal *dst) {
     initial_num(dst);
-    dst->bits[0] = (src > 0) ? src : -src;
+    dst->bits[0] = (src >= 0) ? src : -src;
     set_sign(dst, src < 0);
     return OK;
 }
 
-int s21_from_float_to_decimal(float src, s21_decimal *dst) {
-    
+int get_exp(int number) {
+    int degree_bin = 0, shift = 23, k = 1;
+    while (shift <= 30) {
+        int bit = ((number & (1 << shift)) == 0) ? 0 : 1;
+        degree_bin += bit * k;
+        k *= 10;
+        shift++;
+    }
+
+    int degree = 0, i = 0;
+    while (degree_bin > 0) {
+        degree += degree_bin % 10 * pow(2, i);
+        i++;
+        degree_bin /= 10;
+    }
+
+    return degree;
 }
 
-int s21_from_decimal_to_int(s21_decimal src, int *dst);
+int get_bit_int(int number, int bit) {
+    int sign;
+    if ((number & (1 << bit)) == 0) {
+        sign = 0;
+    } else {
+        sign = 1;
+    }
+    return sign;
+}
+
+int s21_from_float_to_decimal(float src, s21_decimal *dst) {
+    initial_num(dst);
+    set_sign(dst, src < 0);
+    int src_int = *(int*)&src;
+    
+    int exp = get_exp(src_int) - 127;
+    printf("exp = %d\n", exp);
+    dst->bits[0] = 1;
+    src -= pow(2, exp);
+    while (exp > 0) {
+        shift_left(dst);
+        exp--;
+    }
+    
+
+
+    // print_decimal_binary(*dst);
+
+    return 0;
+}
+
+int s21_from_decimal_to_int(s21_decimal src, int *dst) {
+    *dst = 0;
+    *dst = (get_sign(src)) ? -src.bits[0]: src.bits[0];
+    return OK;
+}
+
 int s21_from_decimal_to_float(s21_decimal src, float *dst);
--- main.c
+++ main.c
@@ -171,33 +171,27 @@ void s21_mul_simple(s21_decimal value_1, s21_decimal value_2, s21_decimal *resul
 // }
 
 int main() {
-    s21_decimal num = {0}; //, num2 = {0}, res; 
-    num.bits[0] = 465;
-    num.bits[1] = 0;
-    num.bits[2] = 0;
-    num.bits[3] = 1946531;
+    s21_decimal num; //, num2 = {0}, res; 
+    s21_from_float_to_decimal(1536, &num);
+    // num.bits[0] = 465;
+    // num.bits[1] = 0;
+    // num.bits[2] = 0;
+    // num.bits[3] = 1946531;
 
-    // num2.bits[0] = 2;
-    // num2.bits[1] = 0;
-    // num2.bits[2] = 0;
-    // num2.bits[3] = 0;
+    // // num2.bits[0] = 2;
+    // // num2.bits[1] = 0;
+    // // num2.bits[2] = 0;
+    // // num2.bits[3] = 0;
 
-    // set_degree(&num, 28);
-    // printf("degree = %d\n", get_degree(num));
-
-    // printf("num = %d", num.bits[3]);
-    // set_bit(&num, 2, 0);
-
-
-    print_decimal_binary(num);
-    // print_decimal_binary(num2);
-    // s21_mul_simple(num, num2, &res);
-    shift_right(&num);
-    print_decimal_binary(num);
-    // print_decimal_binary(res);
+    // print_decimal_binary(num);
+    // // print_decimal_binary(num2);
+    // // s21_mul_simple(num, num2, &res);
+    // shift_right(&num);
+    // print_decimal_binary(num);
+    // // print_decimal_binary(res);
 
 
-    // printf("\n%d", get_bit(num, 33));
+    // // printf("\n%d", get_bit(num, 33));
 
     return 0;
 }
