dd4ed13fbb8e04ee3f05e8eae588301d1045e2bc
Todd Redwyne <toddredw@il-e5.msk.21-school.ru>
2022-07-06 17:26:59 +0300

    Fix div and add more bugs

--- main.c
+++ main.c
@@ -4,47 +4,50 @@
 
 
 
-// int main() {
-//   s21_decimal num, num2, res;
-//   s21_from_int_to_decimal(5, &num);
-//   s21_from_int_to_decimal(500, &num2);
-//   set_degree(&num, 0);
+int main() {
+    s21_decimal num, num2, res;
+    // s21_from_int_to_decimal(8784999.87878, &num);
+    s21_from_int_to_decimal(1, &num2);
+    s21_div_simple(num, num2, &res);
+//     print_decimal_binary_top(num);
+//     // s21_from_int_to_decimal(500, &num2);
+//     s21_round(num, &res);
+// //   set_degree(&num, 0);
 //   set_degree(&num2, 2);
 //   s21_add(num, num2, &res);
 //   print_decimal_binary_top(res);
 //   printf("exp = %d\n", get_degree(res));
 
-// //   print_decimal_binary_top(num);
-// //   print_decimal_binary_top(num2);
+//   print_decimal_binary_top(num);
+//   print_decimal_binary_top(num2);
 
-//   //   s21_from_int_to_decimal(1, &num);
-//   //   s21_from_int_to_decimal(2, &num2);
-//   //   float a = 5.41;
-//   //   printf("float = %f\n", a);
+  //   s21_from_int_to_decimal(1, &num);
+  //   s21_from_int_to_decimal(2, &num2);
+  //   float a = 5.41;
+  //   printf("float = %f\n", a);
 
-//   //   s21_from_float_to_decimal(a, &num);
-//   // ostatok(num, num2, &res);
-//   // print_decimal_binary_top(res);
+  //   s21_from_float_to_decimal(a, &num);
+  // ostatok(num, num2, &res);
+    print_decimal_binary_top(res);
 
-//   //   num.bits[0] = 465;
-//   //   num.bits[1] = 0;
-//   //   num.bits[2] = 0;
-//   //   num.bits[3] = 1946531;
+  //   num.bits[0] = 465;
+  //   num.bits[1] = 0;
+  //   num.bits[2] = 0;
+  //   num.bits[3] = 1946531;
 
-//   // // num2.bits[0] = 2;
-//   // // num2.bits[1] = 0;
-//   // // num2.bits[2] = 0;
-//   // // num2.bits[3] = 0;
+  // // num2.bits[0] = 2;
+  // // num2.bits[1] = 0;
+  // // num2.bits[2] = 0;
+  // // num2.bits[3] = 0;
 
-//   //   print_decimal_binary(num);
+  //   print_decimal_binary(num);
 
-//   // print_decimal_binary(num2);
-//   // s21_mul_simple(num, num2, &res);
-//   //   shift_right(&num);
-//   //   print_decimal_binary(num);
-//   // print_decimal_binary(res);
+  // print_decimal_binary(num2);
+  // s21_mul_simple(num, num2, &res);
+  //   shift_right(&num);
+  //   print_decimal_binary(num);
+  // print_decimal_binary(res);
 
-//   // // printf("\n%d", get_bit(num, 33));
-
-//   return 0;
-// }
+  // // printf("\n%d", get_bit(num, 33));
+    return 0;
+}
--- other_function.c
+++ other_function.c
@@ -1,20 +1,59 @@
 #include "s21_decimal.h"
 
 void s21_bank_rounding(s21_decimal *value, int count) {
-  while (count > 0) {
-    s21_decimal base = {0}, one = {0}, two = {0}, two_res = {0};
-    s21_from_int_to_decimal(10, &base);
-    s21_from_int_to_decimal(1, &one);
-    s21_from_int_to_decimal(2, &two);
-    s21_decimal dec_mod = s21_div_simple(*value, base, value);
-    if (dec_mod.bits[0] > 5) {
-      s21_add_simple(*value, one, value);
-    } else if (dec_mod.bits[0] == 5) {
-      two_res = s21_div_simple(*value, two, NULL);
-      if (s21_is_equal_simple(one, two_res)) s21_add_simple(*value, one, value);
+    int sign = get_sign(*value);    
+    while (count > 0) {
+        s21_decimal base = {0}, one = {0}, two = {0}, two_res = {0};
+        s21_from_int_to_decimal(10, &base);
+        s21_from_int_to_decimal(1, &one);
+        s21_from_int_to_decimal(2, &two);
+        s21_decimal dec_mod = s21_div_simple(*value, base, value);
+        if (dec_mod.bits[0] > 5) {
+            s21_add_simple(*value, one, value);
+        } else if (dec_mod.bits[0] == 5) {
+            two_res = s21_div_simple(*value, two, NULL);
+        if (s21_is_equal_simple(one, two_res)) s21_add_simple(*value, one, value);
       // initial_num(&two_res);
     }
     // print_decimal_binary_top(*value);
     count--;
-  }
+    }
+    set_sign(value, sign);
 }
+
+int s21_floor(s21_decimal value, s21_decimal *result) {
+    int sign = get_sign(value);    
+    s21_decimal base = {0};
+    s21_decimal one = {0};
+    s21_from_int_to_decimal(10, &base);  
+    s21_from_int_to_decimal(1, &one);
+    s21_div_simple(value, base, result);
+    if (sign == 1) {
+        s21_add(*result, one, result);
+    }
+    set_sign(result, sign);
+    // print_decimal_binary_top(*result);
+    return 0; // исправить return 
+}
+int s21_round(s21_decimal value, s21_decimal *result) {
+    int sign = get_sign(value);        
+    s21_decimal base = {0}, one = {0}, two = {0}, two_res = {0};
+    s21_from_int_to_decimal(10, &base);
+    s21_from_int_to_decimal(1, &one);
+    s21_from_int_to_decimal(2, &two);
+    s21_decimal dec_mod = s21_div_simple(value, base, result);
+    if (dec_mod.bits[0] >= 5) {
+        s21_add_simple(*result, one, result);
+    }
+    set_sign(result, sign);
+    return 0; // исправить return 
+    // print_decimal_binary_top(*result);
+}
+int s21_truncate(s21_decimal value, s21_decimal *result) {
+    s21_decimal one = {0};
+    s21_from_int_to_decimal(1, &one);
+    s21_div_simple(value, one, result);
+}
+// int s21_negate(s21_decimal value, s21_decimal *result) {
+
+// }
